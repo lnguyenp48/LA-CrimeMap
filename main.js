395bbcb7d1aa2ad7cd62fdb4f4bab55da8870fc9
NatMag22 <natmagcollege@gmail.com>
2025-05-26 13:18:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 // TODO: margins for each section here
+let mapLeft = 0, mapTop = 0
+let mapMargin = {top: 0, right: 0, bottom: 0, left: 0},
+    mapWidth = width,
+    mapHeight = height;
+//Modify margins for main map as needed
 
+let barColumnLeft = mapLeft + 50, barColumnTop = 0
+let barColumnMargin = {top: 0, right: 0, bottom: 0, left: 0},
+    barColumnWidth = width/4 - mapLeft,
+    barColumnHeight = height;
+//Modify margins for bar columns as needed
+
+//Likely need more margins
 
 /*
  * MAIN PLOTS
@@ -43,6 +55,27 @@ d3.csv("Crime_Data_from_2020_to_Present.csv").then(rawData => {
         d.LON = Number(d.LON);
     })
 
+    console.log(rawData); //To verify that the code runs correctly up to this point
+
+    const processedData = rawData.map(d=>{
+        return {
+            //Colors
+
+            //Map
+
+            //Timeline (? Unsure if it needs this)
+
+            //Bar Charts
+            "Vict_Age": d.Vict_Age,
+            "Vict_Sex": d.Vict_Sex
+            //Add more things to bar chart, need to check which data means what
+                
+            //Alluvial Diagriam 
+            };
+        });
+    console.log("processedData", processedData); //To verify that the data was processed correctly via inspecting
+    //Unsure if credit the HW3 that this was based on^
+
     // TODO: data parsing (as needed)
 
     // create svg
@@ -53,6 +86,29 @@ d3.csv("Crime_Data_from_2020_to_Present.csv").then(rawData => {
     // TIMELINE SCROLLING:
 
     // ADDITIONAL PLOTS:
+    //Bar Charts - Victim Age
+    //process data here, check which location is the one that should be used
+    //Processing data and amount for Victim Age
+    const victAgeCounts = processedData.reduce((s, { Vict_Age }) => (s[Vict_Age] = (s[Vict_Age] || 0) + 1, s), {});
+    const victAgeData = Object.keys(victAgeCounts).map((key) => ({ Vict_Age: key, count: victAgeCounts[key] }));
+    console.log("victAgeData", victAgeData);
+    //CHECK WHICH LOCATION IS THE ONE USED HERE [Likely Area]
+
+    barChartVicAge = svg.append("g")
+        .attr("width", barColumnWidth + barColumnMargin.left + barColumnMargin.right)
+        .attr("height", barColumnHeight + barColumnMargin.top + barColumnHeight.bottom)
+        .attr("transform", `translate(${barColumnMargin.left}, ${barColumnTop})`)
+
+    
+    //Bar Charts - Victim Sex
+    barChartVicSex = svg.append("g")
+        .attr("width", barColumnWidth + barColumnMargin.left + barColumnMargin.right)
+        .attr("height", barColumnHeight + barColumnMargin.top + barColumnHeight.bottom)
+        .attr("transform", `translate(${barColumnMargin.left}, ${barColumnTop})`)
+        //Likely add some multiplier to the width/heights/etc. to make sure the spacing is consitient, wait until they can show up to experiment
+    
+    //Referenced HW3 for the Bar Charts [add more official looking reference later]
+
 
     // ALLUVIAL PLOT:
 
