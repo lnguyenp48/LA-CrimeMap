60794dc33d3f3b9100a0674147b2558329397d49
lnguyenp48 <lypnguyen@ucdavis.edu>
2025-06-05 04:48:23 -0700

    nodes + their transitions
    still need to add bar chart connections but otherwise good

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { initMap } from './heatmap.js';
 import { loadCrimeData } from './main.js';
+import { countCrimes } from './main.js';
 
 async function createDashboard() {
     try {
         
         const crimeData = await loadCrimeData();
+
+        const crimeCount = await countCrimes();
        
-        const map = await initMap(crimeData);
+        const map = await initMap(crimeData, crimeCount);
 
     
     } catch (error) {
